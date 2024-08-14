14f680409280ec6fe018fdb63737e833414a5faa
dd <dd@DESKTOP-M360F1M>
2024-08-13 23:57:48 -0500

    stuff

--- src/extension.ts
+++ src/extension.ts
@@ -9,16 +9,16 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ts-ignore-quick-fix" is now active! 11');
+	//console.log('"ts-ignore-quick-fix" extension activated');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('ts-ignore-quick-fix.helloWorld', () => {
+	//const disposable = vscode.commands.registerCommand('ts-ignore-quick-fix.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ts-ignore Quick Fix!');
-	});
+	//	vscode.window.showInformationMessage('Hello World from ts-ignore Quick Fix!');
+	//});
 
 	context.subscriptions.push(
 		vscode.languages.registerCodeActionsProvider(
